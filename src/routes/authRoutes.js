8e9219594aa37ada58e53d52304f0749a10ad39b
laiscoutinho <laiscc575@gmail.com>
2026-02-24 16:59:51 -0300

    feat: personalizando documentação de autenticação

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Cadastrar usuário
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 enum: [ADMIN, VENDEDOR]
+ */
+router.post('/register', authController.register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
+router.post('/login', authController.login);
+
+module.exports = router;
